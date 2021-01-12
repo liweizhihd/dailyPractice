cfdd795c4cd47332f26bea0f3051dae24d391788
liweizhihd <773017708@qq.com>
2021-01-12 21:21:18 +0800

    daily

---
+++ src/main/java/others/forkjoin/ForLoopCalculator.java
@@ -0,0 +1,18 @@
+package others.forkjoin;
+
+import java.math.BigDecimal;
+
+/**
+ * @author liweizhi
+ * @date 2020/12/31
+ */
+public class ForLoopCalculator implements Calculator {
+    @Override
+    public BigDecimal factorial(long number) {
+        BigDecimal ret = new BigDecimal(1);
+        for (long i = 1; i <= number; i++) {
+            ret = ret.multiply(BigDecimal.valueOf(i));
+        }
+        return ret;
+    }
+}
